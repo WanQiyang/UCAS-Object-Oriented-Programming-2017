fb04f6f33665e87e60371aabaa1cd929597eeba4
WanQiyang <qiyangwan@outlook.com>
2017-09-26 20:01:48 +0800

    Update Main.java

--- Assignment1/LuckyMoney/src/com/ribonucleic/Main.java
+++ Assignment1/LuckyMoney/src/com/ribonucleic/Main.java
@@ -14,7 +14,8 @@ class LuckyMoney {
     }
 
     public BigDecimal getLuckyMoney() {
-        if(_num <= 1) return _money;
+        if(_num <= 0) return new BigDecimal(0);
+        if(_num == 1) return _money;
         Random r = new Random();
         double min = 0.01;
         double max = 2 * _money.doubleValue() / _num;
